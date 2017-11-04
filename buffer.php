f0980e7d0d271cb969b1a7e90fb5117ce6b97be6
sluo6 <shangwenluo6@gmail.com>
2017-11-03 20:57:56 -0500

    nail down optimization finished

--- buffer.php
+++ buffer.php
@@ -19,14 +19,14 @@ Lab Calculator for Dummies!
             <h1><a href ="nickel_buffer.php">Buffer for Nickel Column</a></h1>
             <h1><a href ="gel_buffer.php">Buffer for Gel Filtration</a></h1>
             <h1><br></h1>
-            <h1><a href ="two_ph.php">Mixing two buffer stocks for different pH</a></h1>
+            <h1><a href ="two_ph.php">Mixing Two Buffer Stocks for Different pH</a></h1>
             <h1><br></h1>
             <h1><a href ="bradford.php">Bradford Assay</a></h1>
-            <h1><a href ="convert_protein.php">Convert concentration from mg/mL to mM</a></h1>
+            <h1><a href ="convert_protein.php">Convert Concentration from mg/mL to mM</a></h1>
             <h1><br></h1>
-            <h1><a href ="dilution.php">From stocking solution to working concentration</a></h1>
+            <h1><a href ="dilution.php">Nail Down Your Optimization!</a></h1>
             <h1>
-                <a target="_blank" href ="https://www.graphpad.com/quickcalcs/Molarityform/">Making molar solutions</a>
+                <a target="_blank" href ="https://www.graphpad.com/quickcalcs/Molarityform/">Making Molar Solutions</a>
             </h1>
 
         </div>
--- dilution.php
+++ dilution.php
@@ -1,3 +1,84 @@
+<?php
+if(!isset($_POST['action'])){
+    $salt_name="";
+    $salt_stock="";
+    $salt_desired="";
+    $salt_volume="";
+
+    $buffer_name="";
+    $buffer_stock="";
+    $buffer_desired="";
+    $buffer_volume="";
+
+    $precipitant_name="";
+    $precipitant_stock="";
+    $precipitant_desired="";
+    $precipitant_volume="";
+
+    $additive_name="";
+    $additive_stock="";
+    $additive_desired="";
+    $additive_volume="";
+
+    $water_volume="";
+    $total_volume="";
+}elseif($_POST['action']=="Submit"){
+    $total_volume=$_POST['total_volume'];
+
+    $salt_name=$_POST['salt_name'];
+    $salt_stock=$_POST['salt_stock'];
+    $salt_desired=$_POST['salt_desired'];
+    if(($salt_desired=="")||($salt_desired==0)){$salt_volume=0;}
+    else{$salt_volume=$total_volume*$salt_desired/$salt_stock*1000;}
+
+    $buffer_name=$_POST['buffer_name'];
+    $buffer_stock=$_POST['buffer_stock'];
+    $buffer_desired=$_POST['buffer_desired'];
+    if(($buffer_desired=="")||($buffer_desired==0)){$buffer_volume=0;}
+    else{$buffer_volume=$total_volume*$buffer_desired/$buffer_stock*1000;}
+
+    $precipitant_name=$_POST['precipitant_name'];
+    $precipitant_stock=$_POST['precipitant_stock'];
+    $precipitant_desired=$_POST['precipitant_desired'];
+    if(($precipitant_desired=="")||($precipitant_desired==0)){$precipitant_volume=0;}
+    else{$precipitant_volume=$total_volume*$precipitant_desired/$precipitant_stock*1000;}
+
+    $additive_name=$_POST['additive_name'];
+    $additive_stock=$_POST['additive_stock'];
+    $additive_desired=$_POST['additive_desired'];
+    if(($additive_desired=="")||($additive_desired==0)){$additive_volume=0;}
+    else{$additive_volume=$total_volume*$additive_desired/$additive_stock*1000;}
+
+    $total_volume=number_format($_POST['total_volume'],3);
+
+    $water_volume=$total_volume*1000-$salt_volume-$buffer_volume-$precipitant_volume-$additive_volume;
+
+}elseif($_POST['action']=="Reset"){
+    $salt_name="";
+    $salt_stock="";
+    $salt_desired="";
+    $salt_volume="";
+
+    $buffer_name="";
+    $buffer_stock="";
+    $buffer_desired="";
+    $buffer_volume="";
+
+    $precipitant_name="";
+    $precipitant_stock="";
+    $precipitant_desired="";
+    $precipitant_volume="";
+
+    $additive_name="";
+    $additive_stock="";
+    $additive_desired="";
+    $additive_volume="";
+
+    $water_volume="";
+    $total_volume="";
+}
+?>
+
 <!DOCTYPE html>
 <!--
 Design by Shangwen Luo
@@ -16,13 +97,81 @@ Lab Calculator for Dummies!
     <?php include("header_buffer.php") ?>
     <div class="container">
         <form action="dilution.php" method="post" enctype="multipart/form-data">
-            <fieldset>
-                <div id="banner" class="container">
-                    <div class="title">
-                        <h2>UNDER CONSTRUCTION...</h2>
-                    </div>
+            <div style="padding:0 0 0 110px;">
+                <table class="dilution">
+                    <tr class="dilution" style="border-bottom: 3px solid black;">
+                        <td class="dilution">&nbsp;</td>
+                        <td class="dilution">Name (Optional)</td>
+                        <td class="dilution">Stock Conc.</td>
+                        <td class="dilution">Desired Conc.</td>
+                        <td class="dilution">Volume (uL)</td>
+                    </tr>
+                    <tr class="dilution">
+                        <td class="dilution2">Salt</td>
+                        <td class="dilution"><input type="text" name="salt_name"
+                                                    value = "<?php echo $salt_name;?>" class="dilution"/></td>
+                        <td class="dilution"><input type="number" name="salt_stock" step="0.001"
+                                                    value = "<?php echo $salt_stock;?>" min="0" class="dilution2"/>M</td>
+                        <td class="dilution"><input type="number" name="salt_desired" step="0.001"
+                                                    value = "<?php echo $salt_desired;?>" min="0" class="dilution2"/>M</td>
+                        <td class="dilution"><?php echo $salt_volume;?></td>
+                    </tr>
+                    <tr class="dilution">
+                        <td class="dilution2">Buffer</td>
+                        <td class="dilution"><input type="text" name="buffer_name"
+                                                    value = "<?php echo $buffer_name;?>" class="dilution"/></td>
+                        <td class="dilution"><input type="number" name="buffer_stock" step="0.001"
+                                                    value = "<?php echo $buffer_stock;?>" min="0" class="dilution2"/>M</td>
+                        <td class="dilution"><input type="number" name="buffer_desired" step="0.001"
+                                                    value = "<?php echo $buffer_desired;?>" min="0" class="dilution2"/>M</td>
+                        <td class="dilution"><?php echo $buffer_volume;?></td>
+                    </tr>
+                    <tr class="dilution">
+                        <td class="dilution2">Precipitant</td>
+                        <td class="dilution"><input type="text" name="precipitant_name"
+                                                    value = "<?php echo $precipitant_name;?>" class="dilution"/></td>
+                        <td class="dilution"><input type="number" name="precipitant_stock" step="0.001"
+                                                    value = "<?php echo $precipitant_stock;?>" min="0" class="dilution2"/>%</td>
+                        <td class="dilution"><input type="number" name="precipitant_desired" step="0.001"
+                                                    value = "<?php echo $precipitant_desired;?>" min="0" class="dilution2"/>%</td>
+                        <td class="dilution"><?php echo $precipitant_volume;?></td>
+                    </tr>
+                    <tr class="dilution">
+                        <td class="dilution2">Additive (Optional)</td>
+                        <td class="dilution"><input type="text" name="additive_name"
+                                                    value = "<?php echo $additive_name;?>" class="dilution"/></td>
+                        <td class="dilution"><input type="number" name="additive_stock" step="0.001"
+                                                    value = "<?php echo $additive_stock;?>" min="0" class="dilution2"/>%</td>
+                        <td class="dilution"><input type="number" name="additive_desired" step="0.001"
+                                                    value = "<?php echo $additive_desired;?>" min="0" class="dilution2"/>%</td>
+                        <td class="dilution"><?php echo $additive_volume;?></td>
+                    </tr>
+                    <tr class="dilution" style="border-bottom: 3px solid black;">
+                        <td class="dilution2">Water</td>
+                        <td class="dilution"></td>
+                        <td class="dilution"></td>
+                        <td class="dilution"></td>
+                        <td class="dilution"><?php echo $water_volume;?></td>
+                    </tr>
+                    <tr class="dilution">
+                        <td class="dilution2">Total Volume (mL)</td>
+                        <td class="dilution"></td>
+                        <td class="dilution"></td>
+                        <td class="dilution"></td>
+                        <td class="dilution"><input type="number" name="total_volume" step="0.001" required
+                                                    value = "<?php echo $total_volume;?>" min="0" class="dilution2"/></td>
+                    </tr>
 
-            </fieldset>
+
+                </table>
+                <table style="padding: 10px 0 0 0;">
+                    <tr>
+                        <td><input type = "submit" name = "action" value = "Submit">
+                            <input type = "submit" name = "action" value = "Reset"></td>
+                    </tr>
+                </table>
+
+            </div>
         </form>
     </div>
 </div>
